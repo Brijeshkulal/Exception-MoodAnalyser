d79afde729efa931e62d385c73922af1bc737ab5
“Brijesh” <brijeshkulal5@gmail.com>
2021-11-02 08:14:16 +0530

    Added the mood wether it is happy or sad

--- src/main/java/com/labz/mood/MoodAnalyzer.java
+++ src/main/java/com/labz/mood/MoodAnalyzer.java
@@ -2,8 +2,21 @@ package com.labz.mood;
 
 public class MoodAnalyzer {
 	
-	public static void main(String[] args) {
-		System.out.println("welcome to mood analyzer");
+	public String analyseMood(String message) {
+		if(message.contains("Happy")) {
+		return "Happy";
+		}
+		else if(message.contains("Sad")) {
+			return "sad";
+		}
+		return message;
 	}
-
+	
+	public static void main(String[] args) {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String mood = moodAnalyzer.analyseMood("I am happy");
+		System.out.println(mood);
+		mood = moodAnalyzer.analyseMood("I am sad");
+		System.out.println(mood);
+	}
 }
